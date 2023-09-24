3e9f62889e177bbf378f1f6aa23ed242522e4267
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-09-24 11:27:43 +0200

    Login and SignUp using Google Provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,10 +5,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-console.log("process.env.GOOGLE_CLIENT_ID " + process.env.GOOGLE_CLIENT_ID);
-console.log("process.env.GOOGLE_CLIENT_SECRET " + process.env.GOOGLE_CLIENT_SECRET);
-console.log("process.env.secret " + process.env.secret);
-
 export default NextAuth({
   secret: process.env.secret,
   adapter: PrismaAdapter(prisma),
