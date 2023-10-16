8fa33626f43b5ba099b3ef2d87dadc8b55ca089e
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-10-16 20:41:17 +0200

    login added

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
       {
         source: '/api/register',
         destination: 'https://swappify-auth.vercel.app/api/register',
+      },
+      // Proxy for the user Login POST request
+      {
+        source: '/api/login',
+        destination: 'https://swappify-auth.vercel.app/api/login',
       }
     ]
   }
